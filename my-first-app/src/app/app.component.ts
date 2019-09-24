f1452a0767da2d1c14e5aa3adbf6baba94d81c6b
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-09-24 16:22:42 -0300

    add some tests to study

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   name = 'Gustavo';
+
+  onClickButton(buttonData: {oldName: string}) {
+    console.log(buttonData)
+  }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
@@ -7,25 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
-  serverCreationStatus: string = 'No server was created!'
-  serverName: string = ''
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = '';
+  buttonClicked: boolean = false;
+  servers: Array<string> = ['Server 1']
+  @Input('nameOfElement') element: {firstName: string, lastName: string, age: number};
+  @Output() buttonClickedEvent = new EventEmitter<{oldName: string}>();
 
   constructor() {
-    setTimeout (() => {
-      this.allowNewServer = true;
-    }, 2000)
+    this.allowNewServer = true;
   }
 
   ngOnInit() {
   }
 
-  onCreateServer () {
-    this.serverCreationStatus = 'Server was created! ' + this.serverName;
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! ';
+    this.servers.push(this.serverName);
+    this.buttonClicked = !this.buttonClicked;
+    this.buttonClickedEvent.emit({
+      oldName: 'Gustavo'
+    })
   }
 
   onUpdateServerName (/* event: any */ event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
     // this.serverName = event.target.value
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
-
 }
