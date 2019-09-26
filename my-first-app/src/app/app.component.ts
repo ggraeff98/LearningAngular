9fd1b0ba1d9e3e4936e89ec6797a089b573fa013
Gustavo Graeff <gustavograeff1998@gmail.com>
2019-09-26 13:22:22 -0300

    set dafult project template

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  name = 'Gustavo';
 
-  onClickButton(buttonData: {oldName: string}) {
-    console.log(buttonData)
-  }
 }
